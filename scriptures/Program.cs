e7ad5e4496d2b2d3a9efacfaaed899651e6b9c8a
gasolina90 <moralerick90@gmail.com>
2023-02-02 12:35:18 -0700

    modified files

--- scriptures/Program.cs
+++ scriptures/Program.cs
@@ -3,9 +3,18 @@ using System.IO;
 
 public class Program
 {
+    static string someText = "==Here is some text to be added after the original text==";
     static void Main()
     {
+        // Constructors
         File f = new File();
-        f.read_from_file();        
+
+        f.read_from_file();
+
+        f.write_to_file(someText);
+
+        // Console.WriteLine($"This is the scripture text: {f.get_scrip_text()}");
+        // Console.WriteLine("===================================================================================");
+        // Console.WriteLine($"This is the scripture reference: {f.get_scrip_ref()}");
     }
 }
